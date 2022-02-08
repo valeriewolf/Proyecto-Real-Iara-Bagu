87299a1cad91ef15e91a3f1250d612c19b967f47
Juani Quattrini <juaniquattrini@gmail.com>
2022-02-08 01:23:31 -0300

    Slider v1

--- js/app.js
+++ js/app.js
@@ -28,17 +28,49 @@ $('.close-modal').click(function(){
 //MODAL
 
 
-//Carousel
+//Carousel COMENTO TU CODIGO PARA PROBAR QUE ESTA ROTO
+
+//window.addEventListener('load', function(){
+ // new Glider(document.querySelector('.carousel-lista'), {
+   // slidesToShow: 5,
+   // slidesToScroll: 5,
+//dots: '.carousel-indicadores',
+   // arrows: {
+//prev: '.carousel-anterior',
+    //  next: '.carousel-siguiente'
+//}
+ // } );
+
+//});
+
+//
 
 window.addEventListener('load', function(){
-  new Glider(document.querySelector('.carousel-lista'), {
-    slidesToShow: 5,
-    slidesToScroll: 5,
-    dots: '.carousel-indicadores',
-    arrows: {
-      prev: '.carousel-anterior',
-      next: '.carousel-siguiente'
-    }
-  } );
-
+	new Glider(document.querySelector('.carousel__lista'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: '.carousel__indicadores',
+		arrows: {
+			prev: '.carousel__anterior',
+			next: '.carousel__siguiente'
+		},
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 450,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: 2,
+				slidesToScroll: 2
+			  }
+			},{
+			  // screens greater than >= 1024px
+			  breakpoint: 800,
+			  settings: {
+				slidesToShow: 4,
+				slidesToScroll: 4
+			  }
+			}
+		]
+	});
 });
